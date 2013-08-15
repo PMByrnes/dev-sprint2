1a214a14b0d98e8d5431707eca74c79948a190f1
PMByrnes <patrick.m.byrnes@gmail.com>
2013-08-15 00:15:13 -0400

    had some difficulty getting ex 7.5 to work properly

--- chap6.py
+++ chap6.py
@@ -1,9 +1,43 @@
-# Enter your answrs for chapter 6 here
-# Name:
+# Enter your answers for chapter 6 here
+# Name: Pat Byrnes (PMByrnes)
 
 
 # Ex. 6.6
+#1: Calling middle with two letters produces just quotation marks.
+#Same result for both 1 letter and none.
 
+def first(word):
+	return word[0]   #gets the first letter of the string
+def last(word):
+	return word[-1]  #gets the last letter of the string
+def middle(word):
+	return word[1:-1] #gets all except the first and last
+	
+def is_palendrome(word):
+	word = str.lower(word)
+	if len(word) <=1:
+		return 'This is a Palendrome'
+	if last(word) != first(word):
+		return 'This is not a Palendrome'
+	return is_palendrome(middle(word))
+	
 
 
 # Ex 6.8
+
+def MCD(a, b):
+	if a == b:
+		return 'these are equal'
+	elif a < b:
+		if b%a == 0:
+			return 'The MCD is', a
+		while b%a != 0:
+			a = a-1
+		return 'The MCD is', a
+	else:
+		if a%b == 0:
+			return 'The MCD is', b
+		while b%a != 0:
+			b = b-1
+		return 'The MCD is', b
+				
--- chap7.py
+++ chap7.py
@@ -1,9 +1,21 @@
-# Enter your answrs for chapter 7 here
-# Name:
+# Enter your answers for chapter 7 here
+# Name: Pat Byrnes (PMByrnes)
 
 
 # Ex. 7.5
+from math import factorial, pi
 
-
+def estimate_pi(k):
+	part1 = (2.0*(2**0.5))/9801  #this gets the part of the equation before the Sigma
+	num = factorial(4.0*k) * (1103.0 + 26390.0*k) #numerator
+	den = (factorial(k)**4.0) * (396.0**(4.0*k))  #denominator
+	rs = (part1 *(num/den))  #equation for right side
+	est_pi = 1/rs
+	print est_pi
+	
+"""for some reason I can't get the exact equation to work.
+It seems like if I call estimate_pi(0) I'll get roughly pi.
+But each number above zero seems to return an exponentially large number,
+which makes it difficult to progress on this example."""
 
 # How many iterations does it take to converge?
